f0ba6f2d4e3f7dd17ce0ee6cdd864b1cdc229a0b
Alex Figliolia <alexfigliolia@gmail.com>
2025-07-01 21:05:21 -0400

    Test deploy

--- src/app/(in-app-routes)/profile/page.tsx
+++ src/app/(in-app-routes)/profile/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useCallback, useEffect, useState } from "react";
+import { useClassNames } from "@figliolia/classnames";
 import { Avatar } from "Components/Avatar";
 import { TabsContextProvider } from "Components/Tabs/TabsContext";
 import { GridFilled, GridStroked } from "Icons/Grid";
@@ -29,24 +31,46 @@ const TABS: ProfileTab[] = [
 ];
 
 export default function Profile(_: Propless) {
+  const [compress, setCompress] = useState(false);
+
+  const onScroll = useCallback(() => {
+    if (window.scrollY >= 10) {
+      setCompress(true);
+    } else {
+      setCompress(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [onScroll]);
+
+  const classes = useClassNames("profile-page", { compress });
+
   return (
     <TabsContextProvider options={TABS}>
-      <div className="profile-banner-image" />
-      <div className="profile-content">
-        <div className="profile-content__header">
-          <Avatar active />
-          <div className="profile-content__user-info">
-            <ProfileBio />
-            <ProfileStats
-              posts={123}
-              following={335}
-              likes={3000000}
-              followers={4353434}
-            />
-            <ProfileTabs />
+      <div className={classes}>
+        <div className="profile-page__banner" />
+        <div className="profile-page__content">
+          <div className="profile-page__content-header">
+            <Avatar active />
+            <div className="profile-page__content-header-bio">
+              <ProfileBio />
+              <ProfileStats
+                posts={123}
+                following={335}
+                likes={3000000}
+                followers={4353434}
+              />
+              <ProfileTabs />
+            </div>
           </div>
+          <ProfileFeed />
         </div>
-        <ProfileFeed />
       </div>
     </TabsContextProvider>
   );
